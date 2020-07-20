3c6340ad99c10412e895753dfb0d4b4ffc7b1c97
Shivesh <mmshivesh@gmail.com>
2020-07-20 10:19:18 +0530

    Add setup.py for setuptools

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    description = readme.read()
+
+setuptools.setup(
+    name="pysnoonotes",
+    version="1.0.1",
+    author="mmshivesh",
+    author_email="",
+    description="A Python wrapper for the Snoonotes API",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mmshivesh/pysnoonotes",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests==2.23.0"
+    ],
+    python_requires='>=3.6',
+)
